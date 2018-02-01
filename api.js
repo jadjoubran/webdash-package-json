9dde3d57eb8eb5d69a558cb683430d9b9ca53702
Jad Joubran <joubran.jad@gmail.com>
2018-02-01 11:00:12 +0100

    Fix package.json path

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ module.exports = {
     routes: {
         get: {
             'package-json': (req, res) => {
-                const package = require('./../package.json');
+                const package = require('./../../package.json');
                 const result = { package };
 
                 res.send(result);

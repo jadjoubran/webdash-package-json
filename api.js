35a7b9e4707c7e93c046bf77d6444939524b65a8
Jad Joubran <joubran.jad@gmail.com>
2018-02-02 22:23:43 +0100

    experiment with actual versions installed

--- api.js
+++ api.js
@@ -2,10 +2,22 @@ module.exports = {
     routes: {
         get: {
             'package-json': (req, res) => {
-                const package = require('./../../package.json');
-                const result = { package };
+                // const package = require('./../../package.json');
+                // const result = { package };
 
-                res.send(result);
+                // res.send(result);
+
+                const exec = require('child_process').exec
+
+                exec('npm list --depth=0 --json', (error, response) => {
+                    if (error !== null) {
+                        console.log('exec error: ' + error);
+                        return;
+                    }
+                    const result = JSON.parse(response).dependencies;
+
+                    res.send({result});
+                });
             }
         }
     }
